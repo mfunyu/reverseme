8a4550713630aa729351c294b026abcc30f76971
mfunyu <mfunyu@student.42lyon.fr>
2023-12-10 17:57:43 +0100

    fix: while condition

--- level2/source.c
+++ level2/source.c
@@ -20,27 +20,23 @@ int	main()
 	char	str2[9];
 	int		index1;
 	int		index2;
+	char	num[4];
 
 	printf("Please enter key: ");
 	if (scanf("%23s", input) != 1)
 		no();
 
-	if (input[0] != '0')
-		no();
-	if (input[1] != '0')
+	if (input[0] != '0' || input[1] != '0')
 		no();
 
-	//fflush(*stdin);
+	fflush(stdin);
 	memset(str2, 0, 9);
 	str2[0] = 'd';
 	index1 = 2;
 	index2 = 1;
-	char	num[4];
 	num[3] = '\0';
-	while (true)
+	while (strlen(str2) < 8 && index1 < (int)strlen(input))
 	{
-		if (strlen(str2) > 8 || index1 >= (int)strlen(input))
-				break;
 		num[0] = input[index1];
 		num[1] = input[index1 + 1];
 		num[2] = input[index1 + 2];
